3a7256f3772e55f0e9e464ed6b1bd073ecc71b28
chrislangeveldt <chrishlangeveldt@gmail.com>
2022-04-12 14:40:14 +0200

    update gitignore

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/test'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # SECRET_KEY = "flask-app-secret-key-change-it"
 # JWT_SECRET_KEY = "jwt-app-secret-key-change-it"
